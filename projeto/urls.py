60e4a74c013adaf27705913d62f31667a0af63c6
magalhaes1988 <magalhaes1988@gmail.com>
2022-06-16 13:31:27 -0300

    main

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contato', contato),
-    path('sobre/', my_view)
+    path('', include('recipes.urls'))  # / Inital Page
 ]
