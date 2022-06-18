c986765aaacfdae5c1b60036e58e8f5a0bc986a4
magalhaes1988 <magalhaes1988@gmail.com>
2022-06-18 08:59:42 -0300

    alteração do search e separacao para o partials

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html', status=202)
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
+    return render(request, 'recipes/pages/home.html')
