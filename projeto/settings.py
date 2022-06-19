73188e51518392939dd6801581a45ba61847e7d6
magalhaes1988 <magalhaes1988@gmail.com>
2022-06-19 19:25:27 -0300

    commit dinamicas

--- projeto/settings.py
+++ projeto/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_PROJECT = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -57,7 +57,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR/'base_template'
+            BASE_DIR/'base_templates',
+            BASE_DIR/'base_template',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,16 @@
+from __localcode.factory import make_recipe
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': [make_recipe()]
+    })
